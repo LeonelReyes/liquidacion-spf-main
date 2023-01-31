e8eea535012c0977647aa70081854570c7146f3b
Leonel Isaac Reyes <leonel.reyes01@gmail.com>
2023-01-31 13:10:22 -0300

    data fns

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App'
 import { MuiPickersUtilsProvider } from '@material-ui/pickers'
 import DateFnsUtils from '@date-io/date-fns'
-import esLocale from 'date-fns/locale/es'
+// import esLocale from 'date-fns/locale/es'
 
 ReactDOM.render(
   <React.StrictMode>
