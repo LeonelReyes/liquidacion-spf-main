1d94e2284e30118bb212e513c9c69af1d19071ed
Leonel Isaac Reyes <leonel.reyes01@gmail.com>
2023-01-31 13:20:55 -0300

    fns date habilitado

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,13 @@ import './index.css'
 import App from './App'
 import { MuiPickersUtilsProvider } from '@material-ui/pickers'
 import DateFnsUtils from '@date-io/date-fns'
-// import esLocale from 'date-fns/locale/es'
+import esLocale from 'date-fns/locale/es'
 
 ReactDOM.render(
   <React.StrictMode>
     <MuiPickersUtilsProvider 
       utils={DateFnsUtils} 
-      // locale={esLocale}
+      locale={esLocale}
     >
       <App />
     </MuiPickersUtilsProvider>
