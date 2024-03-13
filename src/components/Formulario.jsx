17eb72706af260c14a6f00510210359f1e7a9ea7
Leonel Isaac Reyes <leonel.reyes01@gmail.com>
2024-03-13 16:12:32 -0300

    bug marzo

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -206,7 +206,7 @@ const Formulario = ({datosAgente, setDatosAgente}) => {
                     </li>
 
                     <li className="relative">
-                        <input className="sr-only peer" type="radio" value="4" name="radioMes" id="idMarzo"
+                        <input className="sr-only peer" type="radio" value="5" name="radioMes" id="idMarzo"
                         onChange={ (e) => setMesLiqui(e.target.value) }
                         />
                         <label className="flex p-5 bg-white border border-gray-300 rounded-lg cursor-pointer focus:outline-none hover:bg-gray-50 peer-checked:ring-blue-500 peer-checked:ring-2 peer-checked:border-transparent justify-center" htmlFor="idMarzo">Marzo 12 %</label>
